f42fde752f9ee691a5a386217bc7e4a5014559b5
ajoshdee <a.dee@klab.com>
2016-01-18 16:58:05 +0800

    sanity check

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config(object):
 	CSRF_ENABLED = True
 	SECRET_KEY = 'this-really-needs-to-be-changed'
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+	print(os.environ['DATABASE_URL'])
 	
 class ProductionConfig(Config):
 	DEBUG = False
