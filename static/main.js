c11289f4bd493cf2783d77e3bf65f860ab63969f
ajoshdee <a.dee@klab.com>
2016-01-20 12:18:59 +0800

    Changed submit button to angular implementation

--- static/main.js
+++ static/main.js
@@ -6,7 +6,8 @@
 
   .controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
     function($scope, $log, $http, $timeout) {
-
+    $scope.submitButtonText = "Submit";
+    $scope.loading = false;
     $scope.getResults = function() {
 
       $log.log("test");
@@ -18,7 +19,9 @@
         success(function(results) {
           $log.log(results);
           getWordCount(results);
-
+          $scope.wordcounts = null;
+          $scope.loading = true;
+          $scope.submitButtonText = "Loading...";
         }).
         error(function(error) {
           $log.log(error);
@@ -38,6 +41,8 @@
               $log.log(data, status);
             } else if (status === 200){
               $log.log(data);
+              $scope.loading = false;
+              $scope.submitButtonText = "Submit";
               $scope.wordcounts = data;
               $timeout.cancel(timeout);
               return false;
